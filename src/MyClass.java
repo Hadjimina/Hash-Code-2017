d61418719e099a2f9e81a359167e7e393e550162
Chris Buob <chris.buob.drattni@gmail.com>
2017-02-23 17:49:27 +0100

    file

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,10 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class MyClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		System.out.print("hello, wolrd");
-		System.out.println("jou jou");
+		Scanner file = new Scanner(new File("C:\\Programms\\GitHub\\Hash-Code-2017\\src\\text.txt"));
+		int n = file.nextInt();
+		int[] A = new int[n];
+		for (int i = 0; i < n; i++) {
+			A[i] = file.nextInt();
+		}
+
+		PrintWriter wr = new PrintWriter("C:\\Programms\\GitHub\\Hash-Code-2017\\src\\text2.txt");
+		wr.println(Arrays.toString(A));
+		wr.close();
+		file.close();
 	}
 
 }
